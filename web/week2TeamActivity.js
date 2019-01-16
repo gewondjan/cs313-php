e58b97995fe8303140d2037859b97c2ff0855e51
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2019-01-16 14:08:42 -0700

    added week 2 team activity

---
+++ web/week2TeamActivity.js
@@ -0,0 +1,13 @@
+'using-strict'
+function test() {
+    alert("Hello WOlrd");
+}
+
+function clicked() {
+    alert("Clicked");
+
+}
+
+function changeColor() {
+    document.getElementById("div1").style.backgroundColor = document.getElementById("color").value;
+}
