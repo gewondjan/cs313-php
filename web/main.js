ddf07838f9e03cf5716897378dac73e058458205
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2019-01-19 21:30:19 -0700

    added a bunch of different php files

--- web/main.js
+++ web/main.js
@@ -1,20 +1,40 @@
+var divContent = {
+    me: {
+        image: "ryan.PNG",
+        imageAltText: "Picture of Ryan Gewondjan",
+        text: "My name is Ryan Gewondjan. I am from Brentwood, California.<br> I am a dreamer, a coder and I love to have fun."
+    },
+    dreams: {
+        image: "lightbulb.jpg",
+        imageAltText: "Picture of a Lightbulb",
+        text: "Inventors of history have always caught my attention. I will one day invent something, or come up with a new idea. My career in computer science is a step in that direction."
+    }, 
+    code: {
+        image: "code.jpg",
+        imageAltText: "Picture of code",
+        text: "My favorite part of any class is learning the code. I am fascinated with it. So far I have worked with Java, Javascript, Python, C++, C#, Elisp, and SQL."
+    },
+    fun: {
+        image: "innout.jpg",
+        imageAltText: "Picture of Innout Burger",
+        text: "I love having fun! It keeps me feeling alive. Eating at Innout Burger, running, and spending time with friends and family are some of my favorite pasttimes."
+    },
+}
+
 function makeTitleBold() {
     $(".options").hover(function(){
-       // alert("hover actually workds");
         $(this).find(".option-titles").attr("style", "font-size: 3.5em; font-weight: bold;");
     }, function() {
         $(this).find(".option-titles").attr("style", "");
     });
 }
 
-
 function bringInContent() {
     $(".options").click(function() {
         var name = $(this).find(".option-titles").attr("name");
-        var filename = name + ".php";
-        var phpString = `<?php include '${filename}'; ?>`;
-        $("#presentation-banner").html(phpString);
-         alert(phpString);
+        $("#image").attr("src", divContent[name].image);
+        $("#image").attr("alt", divContent[name].imageAltText);
+        $("#descriptive-text").html(divContent[name].text);
     });
 }
 
@@ -22,6 +42,8 @@ $(document).ready(function(){
     makeTitleBold();
     bringInContent();
 });
+
+
 
 
 
