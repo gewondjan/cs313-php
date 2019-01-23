7791a78a72a3fecced498e52c73f653a5a052337
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2019-01-23 13:44:29 -0700

    week 3 assign

---
+++ web/teamActivities/Week3/submitPhp.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Group 6</title>
+    </head>
+    <body>
+    <h1>Welcome <?php echo $_POST["name"]; ?></h1>
+    <p>Your email is: <?php echo "<a href='mailto:" . $_POST["email"] . ">" . $_POST["email"] . "</a>"; ?></p>
+    <p>Your Major is: <?php echo $_POST["major"]; ?></p>
+
+
+    <ul>
+    </ul>
+
+    </body>
+
+</html>
---
+++ web/teamActivities/Week3/week3.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Group 6 Week 3 Team Assignment</title>
+    </head>
+    <body>
+        
+            <?php
+                echo "<form id='form' method='post' action='submitPhp.php'>";
+                    echo "<p>Please enter your name: </p>";
+                    echo "<input type='text' name='name'><br>";
+                    echo "<p>Please enter your email: </p>";
+                    echo "<input type='email' name='email'><br>";
+                    echo "<p>Please select your major: </p><br>";
+                    echo "<input type='radio' name='major' value='CS' checked> Copmuter Science<br>";
+                    echo "<input type='radio' name='major' value='WDD'> Web Design and Development<br>";
+                    echo "<input type='radio' name='major' value='CIT'> Computer Information Technology<br>";
+                    echo "<input type='radio' name='major' value='CE'> Computer Engineering<br>";
+                    echo "<input type='submit'>"
+                echo "</form>";
+            
+                echo "<br>";
+                echo "<textarea rows='4' cols='50' name='comment' form='form'>";
+                echo "Enter comment here...</textarea>";
+                
+            ?>
+    </body>
+</html>
