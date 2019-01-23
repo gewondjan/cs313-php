90eed836aa87f3dac0197d18ae73fce0ecac2098
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2019-01-23 13:57:14 -0700

    committing group assing

--- web/submitPhp.php
+++ web/submitPhp.php
@@ -5,10 +5,10 @@
     </head>
     <body>
     <h1>Welcome <?php echo $_POST["name"]; ?></h1>
-    <p>Your email is: <?php echo "<a href='mailto:" . $_POST["email"] . ">" . $_POST["email"] . "</a>"; ?></p>
+    <p>Your email is: <?php echo "<a href='mailto:" . $_POST["email"] . "'>" . $_POST["email"] . "</a>"; ?></p>
     <p>Your Major is: <?php echo $_POST["major"]; ?></p>
 
-
+    <p>Your Comments: <?php echo $_POST["comment"]; ?> </p>
     <ul>
     </ul>
 
