aab5c77230fe59bc32c321f0218e7cf8f9a4959d
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2019-01-28 14:10:12 -0700

    creating assignment 3

---
+++ web/week3Assign/cart.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Shopping Cart</title>
+
+</head>
+
+<body>
+<h1>Welcome to Cereal Delivery</h1>
+<h2>For the moments when you are in need of a bowl of cereal.</h2>
+<br>
+<h3>Choose from our wide selection of cereals.</h3>
+
+
+<form action="<?php $_SERVER["PHP_SELF"] ?>">
+
+
+    <input type="submit" value="Add to Cart">
+</form>
+
+
+
+</body>
+</html>
