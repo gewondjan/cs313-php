f8bb503ff65c51b4b3bab112f5a062cbf4e04623
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2019-01-29 08:15:19 -0700

    working on week 3 assign

--- web/week3Assign/browse.php
+++ web/week3Assign/browse.php
@@ -29,7 +29,7 @@
         }
 
         foreach ($cereals as $cereal_id => $cereal_name){
-            echo "<input type='checkbox' name='cereals_checked[]' value='$cereal_id'><span>$cereal_name</span>";
+            echo "<input type='checkbox' name='cereals_checked[]' value='$cereal_id'><span>$cereal_name</span><br>";
             //echo "<input list='numbers$cereal_id'><datalist id='numbers$cereal_id'><option value='1'><option value='2'><option value='3'><option value='4'></datalist><br>";
         }
     ?>
