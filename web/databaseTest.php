e94fbcdbafc7d145068a3247c518b745df46b525
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2019-02-08 15:07:15 -0700

    adding database test file

--- web/databaseTest.php
+++ web/databaseTest.php
@@ -1,28 +1,28 @@
 
 <?php
 
-// try {
+try {
 
-//     $url = getenv("DATABASE_URL");
+    $url = getenv("DATABASE_URL");
     
-//     $dbParts = parse_url($url);
+    $dbParts = parse_url($url);
     
-//     $dbPort = $dbParts["port"];
-//     $dbHost = $dbParts["host"];
-//     $dbname = ltrim($dbParts["path"], "/");
-//     $dbUsername = $dbParts["user"];
-//     $dbPassword = $dbParts["pass"];
+    $dbPort = $dbParts["port"];
+    $dbHost = $dbParts["host"];
+    $dbname = ltrim($dbParts["path"], "/");
+    $dbUsername = $dbParts["user"];
+    $dbPassword = $dbParts["pass"];
     
 
     
-//     $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbname", $dbUsername, $dbPassword);
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbname", $dbUsername, $dbPassword);
     
-//     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERROMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERROMODE_EXCEPTION);
     
-// } catch (PDOException $exception) {
-//     echo "Error: " . $exception->getMessage();
-//     die();
-// }
+} catch (PDOException $exception) {
+    echo "Error: " . $exception->getMessage();
+    die();
+}
 
 ?> 
 <!DOCTYPE html>
@@ -33,11 +33,11 @@
 <body>
 <h2>Welcome</h2>
     <?php
-        // $statement = $db->query('SELECT city from weather');
+        $statement = $db->query('SELECT city from weather');
         
-        // while($row = $statement->fetch(PDO::FETCH_ASSOC)){
-        //     echo "<h1>" . $row['city'] . "</h1>";
-        // }
+        while($row = $statement->fetch(PDO::FETCH_ASSOC)){
+            echo "<h1>" . $row['city'] . "</h1>";
+        }
 
     ?>
 
