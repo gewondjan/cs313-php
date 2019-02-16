ceff8a3afb8a37031e76ed34c140b8f87159d055
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2019-02-16 11:17:36 -0700

    working on team assign

---
+++ web/teamActivities/addScripture.php
@@ -0,0 +1,48 @@
+<?php
+
+$url = parse_url(getenv('DATABASE_URL'));
+
+$dbname = ltrim($url['path'], '/');
+$dbHost = $url['host']; 
+$dbPort = $url['port'];
+
+try
+{
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbname", $url['user'], $url['pass']);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Scriptures DB</title>
+</head>
+<body>
+  <h1>Insert Scipture Topics</h1>
+
+  <form method="post" action="insert.php">
+      <input type="text" name="book" placeholder="Enter the Book Name">
+      <input type="text" name="chapter" placeholder="Enter the Chapter Number">
+      <input type="text" name="verse" placeholder="Enter the Verse Number">
+      <textarea row="4" name="content" cols="50" placeholder="Enter Content Here"></textarea>
+
+      <?php
+      $stmt = $db->query('SELECT topic_id, name FROM topic');
+
+      while ($row=$stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo "<input type='checkbox' name='topics[]' value='" . "$row[topic_id]'> $row[name]";
+      }
+       ?>
+    <input type="submit" />
+  </form>
+
+</body>
+</html>
--- web/teamActivities/insert.php
+++ web/teamActivities/insert.php
@@ -1,61 +1,69 @@
 <?php
-  // start session for getting scriptures details
-  session_start();
-  // local DB variable configurations
-  $dbHost = 'localhost';
-  $dbPort = '5432';
-  $dbName = 'scripturesdb';
-  try
-  {
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName");
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  }
-  catch (PDOException $ex)
-  {
-    echo 'Error!: ' . $ex->getMessage();
-    die();
-  }
+
+$url = parse_url(getenv('DATABASE_URL'));
+
+$dbname = ltrim($url['path'], '/');
+$dbHost = $url['host']; 
+$dbPort = $url['port'];
+
+try
+{
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbname", $url['user'], $url['pass']);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Scriptures DB</title>
-    <style>
-      body {
-        display: flex;
-        flex-direction: column;
-      }
-      form {
-        display: flex;
-        flex-direction: column;
-        width: 40%;
-        margin-left: 10%;
-      }
-      input {
-        margin-bottom: 20px;
-        height: 30px;
-      }
-    </style>
 </head>
 <body>
-  <h1>Insert Scipture Topics</h1>
+  <h1>Scripture Topics</h1>
+<?php
+    $stmt = $db->prepare('INSERT INTO scripture (book, chapter, verse, content)
+      VALUES (:book, :chapter, :verse, :content)');
 
-  <form method="post" action="display_scr.php">
-      <input type="text" name="book" placeholder="Enter the Book Name"></input>
-      <input type="text" name="chapter" placeholder="Enter the Chapter Number"></input>
-      <input type="text" name="verse" placeholder="Enter the Verse Number"></input>
-      <textarea row="4" name="content" cols="50" placeholder="Enter Content Here"></textarea>
+    $stmt->bindValue(':book', $_POST['book'], PDO::PARAM_STR);
+    $stmt->bindValue(':chapter', $_POST['chapter'], PDO::PARAM_INT);
+    $stmt->bindValue(':verse', $_POST['verse'], PDO::PARAM_INT);
+    $stmt->bindValue(':content', $_POST['content'], PDO::PARAM_STR);
+    $stmt->execute();
 
-      <?php
-      $stmt = $db->query('SELECT topic_id, name FROM topic');
+    $newID = $pdo->lastInsertId('scripture_scripture_id_seq');
 
-      while ($row=$stmt->fetch(PDO::FETCH_ASSOC)) {
-        echo "<input type='checkbox' name='topics[]' value='" . "$row[topic_id]'> $row[name]";
-      }
-       ?>
-    <input type="submit" />
-  </form>
+    foreach ($_POST['topics'] as $topic) {
+      echo 'scripture_id: scripture_id, topic_id: topic_id';
+      $stmt = $db->prepare('INSERT INTO scripture_topic (scripture_id, topic_id)
+        VALUES (:scripture_id, :topic_id)');
+
+        $stmt->bindValue(':topic_id', $topic);
+        $stmt->bindValue(':scripture_id', $newID);
+        $stmt->execute();
+    }
+
+    $stmt = $db->query('SELECT id, book, chapter, verse from scripture');
+
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+     echo "<p>" . $row['book'] . $row['chapter'] . ":" . $row['verse'] . "<br>" . $row['content'] . "<br></p>";
+
+    $nextStatement = $db->prepare('SELECT name FROM topic JOIN scripture_topic ON topic.topic_id = scripture_topic.topic_id WHERE scripture_topic.scripture_id = :scripture_id');
+
+    $nextStatement->bindValue(":scripture_id", $row['id'], PDO::PARAM_INT);
+    $nextStatement->execute();
+
+    while ($listOfTopics = $nextStatement->fetch(PDO::FETCH_ASSOC)) {
+      echo "<p>" . $listOfTopics['name'] . "</p> <br>";
+    }
+}
+?>
 
 </body>
 </html>
