404d5583acffe5c8ffbae3c997d399ebff4b8110
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2019-02-25 21:46:10 -0700

    adding more to my project

--- web/phpProject/bucketlist.js
+++ web/phpProject/bucketlist.js
@@ -17,23 +17,23 @@ function moveCard(cardId) {
         //All the code below is good for a UI shuffling of the cards, but since the bucketlist.php file will be returned to anyway, this code
         //is unnecessary for now.  It is amazing code though that may be useful at a later time.
         
-        // //Remove the card from it's current location
-        // $(`#${cardId}`).parent().empty();
+        //Remove the card from it's current location
+        $(`#${cardId}`).parent().empty();
 
-        // //Insert the card content to the new location
-        // $(`#${coordinate}`).html(cardContent);
+        //Insert the card content to the new location
+        $(`#${coordinate}`).html(cardContent);
 
-        // //Clear all selected values
-        // $(`#numberPriority${cardId} > option`).attr("selected", "false");
-        // $(`#abcPriority${cardId} > option`).attr("selected", "false");
+        //Clear all selected values
+        $(`#numberPriority${cardId} > option`).attr("selected", "false");
+        $(`#abcPriority${cardId} > option`).attr("selected", "false");
 
-        // //Update the abc and number priorities
-        // $(`#abcOption${abcPriority}-${cardId}`).attr("selected", "selected");
-        // $(`#numberOption${numberPriority}-${cardId}`).attr("selected", "selected");
+        //Update the abc and number priorities
+        $(`#abcOption${abcPriority}-${cardId}`).attr("selected", "selected");
+        $(`#numberOption${numberPriority}-${cardId}`).attr("selected", "selected");
 
-        // //Make sure the correct values are showing.
-        // $(`#abcPriority${cardId}`).val(abcPriority);
-        // $(`#numberPriority${cardId}`).val(numberPriority);
+        //Make sure the correct values are showing.
+        $(`#abcPriority${cardId}`).val(abcPriority);
+        $(`#numberPriority${cardId}`).val(numberPriority);
 
         // alert($(`#${coordinate}`).html());
     
